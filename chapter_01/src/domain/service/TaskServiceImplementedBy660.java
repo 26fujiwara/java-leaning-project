3855a8029485f7340a8f390bf368cff39abda27c
26fujiwara <kenta_fran@yahoo.co.jp>
2019-05-06 01:28:43 +0900

    Add:課題1を実施 fix:課題1を修正 Add:課題2を実施

--- chapter_01/src/domain/service/TaskServiceImplementedBy660.java
+++ chapter_01/src/domain/service/TaskServiceImplementedBy660.java
@@ -7,7 +7,10 @@ public class TaskServiceImplementedBy660 implements ITask {
     // 問１
     @Override
     public void learnJavaMechanism() {
-            System.out.println("JavaプログラムはJavaバイトコードと呼ばれる中間言語（中間表現）にコンパイルされて、Java仮想マシンと呼ばれるソフトウェア上で実行される。各コンピュータ環境に対応したJava仮想マシンがハードウェア間の差異を吸収し、特定の環境に依存しないプログラム動作を実現する仕組みとなっている。");
+            System.out.println("JavaプログラムはJavaバイトコードと呼ばれる中間言語（中間表現）にコンパイルされて、") ;
+            System.out.println("Java仮想マシンと呼ばれるソフトウェア上で実行される。") ;
+            System.out.println("各コンピュータ環境に対応したJava仮想マシンがハードウェア間の差異を吸収し、") ;
+            System.out.println("特定の環境に依存しないプログラム動作を実現する仕組みとなっている。") ;
     }
 
     // 問２
@@ -15,12 +18,9 @@ public class TaskServiceImplementedBy660 implements ITask {
     public void learnVariable() {
         int x = 3;
         int y = 5;
-        int z;
+        int z = x * y;
 
-        z = x * y;
-
-        System.out.println(z);
-
+        System.out.println("縦幅 " + x + " 横幅 " + y + " の長方形の面積は、" + z);
     }
 
 
@@ -33,7 +33,7 @@ public class TaskServiceImplementedBy660 implements ITask {
         x = 100;
         x += x * shouhizei;
 
-        System.out.println(x);
+        System.out.println("商品お買い上げありがとうございます。"+ x +"円になります");
     }
 
     // 問４
